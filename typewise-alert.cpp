3c1a61cb8a165ed782339bd04daf931d10fda517
sricharanlakkoju <73473045+sricharanlakkoju@users.noreply.github.com>
2024-08-27 08:47:00 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,35 +1,32 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
+
+// Function to get the temperature range based on cooling type
+TemperatureRange getTemperatureRange(CoolingType coolingType) {
+  TemperatureRange ranges[] = {
+    {0, 35}, // PASSIVE_COOLING
+    {0, 45}, // HI_ACTIVE_COOLING
+    {0, 40}  // MED_ACTIVE_COOLING
+  };
+  return ranges[coolingType];
+}
+
+// Function to infer breach type based on value and limits
+BreachType inferBreach(double value, TemperatureRange range) {
+  if (value < range.lowerLimit) {
     return TOO_LOW;
   }
-  if(value > upperLimit) {
+  if (value > range.upperLimit) {
     return TOO_HIGH;
   }
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+// Function to classify temperature breach based on cooling type and temperature
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+  TemperatureRange range = getTemperatureRange(coolingType);
+  return inferBreach(temperatureInC, range);
 }
 
 void checkAndAlert(
@@ -53,19 +50,16 @@ void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
-
 void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
+    const char* recipient = "a.b@c.com";
+    const std::unordered_map<BreachType, std::string> messages = {
+        {BreachType::TOO_LOW, "Hi, the temperature is too low"},
+        {BreachType::TOO_HIGH, "Hi, the temperature is too high"}
+    };
+ 
+    auto it = messages.find(breachType);
+    if (it != messages.end()) {
+        printf("To: %s\n", recipient);
+        printf("%s\n", it->second.c_str());
+    }
 }
