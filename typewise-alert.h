42441f2e9eec8128c27949df45c3fd32a2c15cd1
sricharanlakkoju <73473045+sricharanlakkoju@users.noreply.github.com>
2024-08-27 08:52:49 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -12,6 +12,11 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
+typedef struct {
+    BreachType type;
+    const char* message;
+} BreachMessage;
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
